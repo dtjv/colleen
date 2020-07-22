35924ea01a3f2e2f4ef72ac6df3a316feb34ada4
David Valles <davidtjvalles@gmail.com>
2020-07-22 16:09:30 -0700

    Add env vars to build

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
+    require("postcss-import")({
+      filter: (path) => {
+        if (
+          process.env.NODE_ENV === "production" &&
+          path.includes("config.dev")
+        ) {
+          return false;
+        }
+
+        if (
+          process.env.NODE_ENV !== "production" &&
+          path.includes("config.production")
+        ) {
+          return false;
+        }
+
+        return true;
+      },
+    }),
     require("precss"),
     require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
